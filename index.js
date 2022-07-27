6594ed300d44c2db2f560f0f6a4a0f263c147ed4
CeeLew <cameron.lewis.webdev@gmail.com>
2022-07-26 23:03:46 -0400

    Finished exercise three.  Learned a lot!

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function multiply (x, y) {
 }
 
 console.log(multiply(5, 9));
+
+// Exercise 3
+
+// The below function separates the string into two categories: The first letter that will always be capitalized
+// and the rest of the string that will always be lowercase.  After creating a variable for each category
+// I used the return function to put the two categories together.  The first letter will always be capitalized
+// and the rest of the letters in the string will be lowercase even if they are typed as uppercase.
+
+function capitalize (string) {
+  const upr = string[0];
+  const lowr = string.slice(1, );
+  return (upr.toUpperCase() + lowr.toLowerCase())
+}
+
+console.log(capitalize('pROgraMming'));
