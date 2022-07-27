7fbecdda99fa8673ea07a1c6bf9d5f0d3ffa9d3f
CeeLew <cameron.lewis.webdev@gmail.com>
2022-07-26 22:55:29 -0400

    finished exercise 2

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// Exercise 1
+
 function add7 (n) {
   return (n + 7);
 }
 
 console.log(add7(11));
+
+// Exercise 2
+
+function multiply (x, y) {
+  return (x*y);
+}
+
+console.log(multiply(5, 9));
